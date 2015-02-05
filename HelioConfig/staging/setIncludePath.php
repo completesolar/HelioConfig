0d0008ba358b708d214a1921f5029bf3e59cc593
alanma <alanma@d3ea97c9-f87a-4f39-b3ea-c0debacafb09>
2015-02-05 09:21:54 +0000

    Finally Deleting the Old AppointmentAssignment project. Finally Deleting the old Commons Project. Renaming the old HelioConfig Project.

--- HelioConfig/staging/setIncludePath.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CompleteSolar\HelioConfig;
-
-$path = dirname(__FILE__).'/';
-set_include_path(get_include_path().PATH_SEPARATOR."$path../commons");
-require_once "commonsIncludePath.php";
-
-
-?>
