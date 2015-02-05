540789e5c5965894a09bd89a6ed97f314734c0c0
alanma <alanma@d3ea97c9-f87a-4f39-b3ea-c0debacafb09>
2015-02-05 12:27:43 +0000

    Tagging Branch 0.0.1

---
+++ HelioConfig/tags/0.0.1/src/Utilities/Config.php
@@ -0,0 +1,97 @@
+<?php namespace CompleteSolar\HelioConfig\Utilities;
+
+class Config {
+    public $properties;
+    private $useSections;
+    private $configFile;
+    public function __construct($configFile, $useSections = true) {
+        $this->properties = parse_ini_file ( $configFile, $useSections );
+        $this->useSections = $useSections;
+    }
+    
+    public function getProperty($indexArray) {
+        $indexArray = $this->configureIndexArray($indexArray);
+        if (count($indexArray) == 0) {
+            return null;
+        }
+        $val = $this->properties;
+        foreach($indexArray as $index){
+            if (!array_key_exists($index, $val)){
+                throw new \OutOfBoundsException("$index not found in config file.");
+            }
+            $val = $val[$index];
+        }
+        return $val;
+    }
+    
+    public function setProperty($indexArray, $value){    
+        $indexArray = $this->configureIndexArray($indexArray);
+        if (count($indexArray) == 0) {
+            return null;
+        }
+        $val = &$this->properties;
+        for($i=0;$i<count($indexArray)-1; $i++){
+            $index = $indexArray[$i];
+            if (!array_key_exists($index, $this->properties)){
+                throw new \OutOfBoundsException("$index not found in config file.");
+            }
+            $val = &$val[$index];
+        }
+        $val[$indexArray[count($indexArray) - 1]] = $value;
+/*        foreach($indexArray as $index){
+            if (!array_key_exists($index, $this->properties)){
+                throw new \OutOfBoundsException("$index not found in config file.");
+            }
+            $val = $this->properties;
+            $this->properties[$index] = $value;
+        }*/
+    }
+    
+    public function write_ini_file($path) {
+        $content = "";
+        if ($this->useSections) {
+            foreach ($this->properties as $key=>$elem) {
+                $content .= "[".$key."]\n";
+                foreach ($elem as $key2=>$elem2) {
+                    if(is_array($elem2)){
+                        for($i=0;$i<count($elem2);$i++){
+                            $content .= $key2."[] = \"".$elem2[$i]."\"\n";
+                        }
+                    } else if($elem2==""){
+                        $content .= $key2." = \n";
+                    } else {
+                        $content .= $key2." = \"".$elem2."\"\n";
+                    }
+                }
+            }
+        } else {
+            foreach ($this->properties as $key=>$elem) {
+                if(is_array($elem)){
+                    for($i=0;$i<count($elem);$i++){
+                        $content .= $key."[] = \"".$elem[$i]."\"\n";
+                    }
+                }
+                else if($elem==""){
+                    $content .= $key." = \n";
+                } else {
+                    $content .= $key." = \"".$elem."\"\n";
+                }
+            }
+        }
+        if (!$handle = fopen($path, 'w')) {
+            return false;
+        }
+        $success = fwrite($handle, $content);
+        fclose($handle);
+        return $success;
+    }
+    
+    private function configureIndexArray($indexArray){
+        if (!is_array($indexArray)){
+            $indexArray = array (
+                    $indexArray
+            );
+        }
+        return $indexArray;
+    }
+}
